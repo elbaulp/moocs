59a96ca33316b680187a122b0468b7f814c9483d
Alejandro Alcalde <algui91@gmail.com>
2017-05-26 21:18:55 +0200

    Implemented exists

--- progfun1/funsets/src/main/scala/funsets/FunSets.scala
+++ progfun1/funsets/src/main/scala/funsets/FunSets.scala
@@ -80,7 +80,15 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = ???
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    @tailrec
+    def iter(a: Int): Boolean = {
+      if (a > 1000) false
+      else if (s(a) && p(a)) true
+      else iter(a + 1)
+    }
+    iter(-1000)
+  }
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
--- progfun1/funsets/src/test/scala/funsets/FunSetSuite.scala
+++ progfun1/funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -145,12 +145,23 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-  test("Test if p holds for all elements in the range [-1000,1000]") {
+  test("P holds for all elements in the range [-1000,1000]") {
     new TestSets {
       val f = forall(u1234, x => x < 5)
       val f2 = forall(u1234, x => x < 0)
+
       assert(f == true, "All values in {1,2,3} < 5")
       assert(f2 == false, "All values in {1,2,3} > 0")
+    }
+  }
+
+  test("P holds if there exists at least one elements that satisfies p") {
+    new TestSets {
+      val f = exists(u1234, x => x % 2 == 0)
+      val f1 = exists(u1234, x => x < 0)
+
+      assert(f == true, "At least on element in {1,2,3} is multiple of 2")
+      assert(f1 == false, "No element in {1,2,3} < 0")
     }
   }
 }
