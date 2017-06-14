5cc96af6f94bfe80be100dee46902736ed6d266d
Alejandro Alcalde <algui91@gmail.com>
2017-06-14 18:21:54 +0200

    Implemented subtract

--- scala/progfun1/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ scala/progfun1/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -111,7 +111,12 @@ object Anagrams {
    *  Note: the resulting value is an occurrence - meaning it is sorted
    *  and has no zero-entries.
    */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
+  def subtract(x: Occurrences, y: Occurrences): Occurrences =
+    for {
+      l <- x
+      r <- y
+      if (l != r)
+    } yield l
 
   /** Returns a list of all anagram sentences of the given sentence.
    *
