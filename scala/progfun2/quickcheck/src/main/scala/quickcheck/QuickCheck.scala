0f2feb02e049a82b83e8c41cc924ab0c4e134587
Alejandro Alcalde <algui91@gmail.com>
2017-06-29 18:17:00 +0200

    Added generator for the Heap

--- scala/progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ scala/progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -6,15 +6,38 @@ import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
+import Arbitrary.arbitrary
+
+/**
+  * - A property is the testable unit in ScalaCheck.
+  * - forAll, ... defined in Prop
+  * - forAll takes a function and return Boolean or other Prop.
+  * - Implication: ==>, f.e: (n >= 0 && n < 10000) ==> (List.fill(n)("").length == n)
+  * - Properties can be combined: p1 && p2...
+  * - and grouped
+  */
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  val genEmpty = const(empty)
+
+  lazy val genHeap: Gen[H] = oneOf(
+    genEmpty,
+    for {
+      i <- arbitrary[Int]
+      h <- oneOf(genEmpty, genHeap)
+    } yield insert(i, h)
+  )
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
+  }
+
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
   }
 
 }
