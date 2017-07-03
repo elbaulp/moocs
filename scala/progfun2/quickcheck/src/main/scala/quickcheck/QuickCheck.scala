342e93b0bf9ddf8578dec7c927f75e606747542c
Alejandro Alcalde <algui91@gmail.com>
2017-07-03 13:46:17 +0200

    QuickCheck: Order property

--- scala/progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ scala/progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -10,13 +10,13 @@ import Arbitrary.arbitrary
 import scala.annotation.tailrec
 
 /**
-  * - A property is the testable unit in ScalaCheck.
-  * - forAll, ... defined in Prop
-  * - forAll takes a function and return Boolean or other Prop.
-  * - Implication: ==>, f.e: (n >= 0 && n < 10000) ==> (List.fill(n)("").length == n)
-  * - Properties can be combined: p1 && p2...
-  * - and grouped
-  */
+ * - A property is the testable unit in ScalaCheck.
+ * - forAll, ... defined in Prop
+ * - forAll takes a function and return Boolean or other Prop.
+ * - Implication: ==>, f.e: (n >= 0 && n < 10000) ==> (List.fill(n)("").length == n)
+ * - Properties can be combined: p1 && p2...
+ * - and grouped
+ */
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -31,6 +31,8 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   )
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
+  val intSet = containerOfN[Set, Int](3, arbitrary[Int])
+
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
@@ -43,7 +45,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   // If you insert any two elements into an empty heap, finding the minimum of the
   // resulting heap should get the smallest of the two elements back.
-  property("2items2emptyH") = forAll { (a: Int, b:Int) =>
+  property("2items2emptyH") = forAll { (a: Int, b: Int) =>
     val h = insert(a, empty)
     val h1 = insert(b, h)
     val min = a min b
@@ -52,19 +54,18 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   // If you insert an element into an empty heap, then delete the minimum, the
   // resulting heap should be empty.
-  property("itemToEmptyAndRemove") = forAll { a:Int =>
+  property("itemToEmptyAndRemove") = forAll { a: Int =>
     val h = insert(a, empty)
     val eH = deleteMin(h)
     eH == empty
   }
-
 
   // Given any heap, you should get a sorted sequence of elements when
   // continually finding and deleting minima. (Hint: recursion and helper
   // functions are your friends.)
   property("sorted") = forAll { h: H =>
     @tailrec
-    def removeAll(h: H, acc:List[A] = Nil): List[A] = {
+    def removeAll(h: H, acc: List[A] = Nil): List[A] = {
       if (isEmpty(h)) Nil
       else {
         val min = findMin(h)
@@ -96,5 +97,17 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
       "min" |: (a min b) == mi,
       "max" |: (a max b) == ma
     )
+  }
+
+  // Checking the order property
+  property("3elems") = forAll(intSet){ s =>
+    s.size == 3 ==> {
+      val v = s.toList
+      val h = insert(v(0), insert(v(1), insert(v(2), empty)))
+      val min = findMin(h)
+      val h2 = deleteMin(h)
+
+      min < findMin(h2)
+    }
   }
 }
