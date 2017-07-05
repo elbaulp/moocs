8022203b980e875e882afb62ccaa68552bc1b8d1
Alejandro Alcalde <algui91@gmail.com>
2017-07-05 17:46:51 +0200

    Progfun2: Implementing Polynomial with FRP

--- scala/progfun2/calculator/src/main/scala/calculator/Polynomial.scala
+++ scala/progfun2/calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,18 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+    c: Signal[Double]): Signal[Double] = {
+    // bb - 4ac
+    Signal(b() * b() - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal {
+      delta() match {
+        case d if d < 0 => Set()
+        case d => Set((-b() + math.sqrt(d)) / 2 * a(), (-b() - math.sqrt(d)) / 2 * a())
+      }
+    }
   }
 }
